f02d6e3c31c34d5d8840bb3d13879b2c651a9d11
David Woolf <davidjohnwoolf@gmail.com>
2015-10-10 16:57:48 -0700

    add settings, dependencies, middlware, and login view

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+'use strict';
+
 var express = require('express');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var users = require('./controllers/users');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Welcome to Inform');
-});
+// settings
+var port = process.env.PORT || 1337;
+app.set('view engine', 'hbs');
 
-app.listen(3000);
+// database
+mongoose.connect('mongodb://localhost/expressAuthSkeleton');
+
+// middleware
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(session({
+  // to use secure cookies use https and update code
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(function(req, res, next) {
+  // set session and flash info to locals
+  res.locals.session = req.session;
+  res.locals.flashNotice = req.flash('notice');
+  res.locals.flashAlert = req.flash('alert');
+  next();
+});
+app.use(express.static(__dirname + '/public'));
+app.use('/', users);
+
+// server
+app.listen(port);
+console.log('Listening...');
