918dd59125493cd37e9586065cc4d924417aa0be
David Woolf <davidjohnwoolf@gmail.com>
2015-10-10 17:12:08 -0700

    add error handling and view

--- app.js
+++ app.js
@@ -37,6 +37,37 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/public'));
 app.use('/', users);
 
+// error handling (taken from express generator)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 // server
 app.listen(port);
 console.log('Listening...');
