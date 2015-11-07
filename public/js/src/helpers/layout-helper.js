4610941a26d55bcdf6c3e9096a6fbb0dcd272580
David Woolf <davidjohnwoolf@gmail.com>
2015-11-06 16:49:53 -0800

    unload layout components if the arg doesnt exist

--- public/js/src/helpers/layout-helper.js
+++ public/js/src/helpers/layout-helper.js
@@ -11,12 +11,16 @@ function layoutHelper(args) {
       document.getElementById('feed-select'),
       m.component(args.feedSelect, { feeds: args.feeds, currentFeed: args.currentFeed })
     );
+  } else {
+    m.mount(document.getElementById('feed-select'), null);
   }
   if (args.refreshButton) {
     m.mount(
       document.getElementById('refresh-button'),
       m.component(args.refreshButton)
     );
+  } else {
+    m.mount(document.getElementById('refresh-button'), null);
   }
   // if (args.search) {
   //   m.render(document.getElementById('right-header'), [
