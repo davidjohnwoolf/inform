08df23f51a58c03fab2081d0b067f6607ffbbf79
David Woolf <davidjohnwoolf@gmail.com>
2015-11-17 00:50:07 -0800

    remove console log

--- public/js/src/feeds/models/feed-results.js
+++ public/js/src/feeds/models/feed-results.js
@@ -3,7 +3,6 @@ var reqHelpers = require('../../helpers/request-helpers');
 var authorizeHelper = require('../../helpers/authorize-helper');
 
 var FeedResults = function() {
-  console.log(m.route.param('feedId'));
   return m.request({
     method: 'GET',
     url: '/users/' + m.route.param('id') + '/feeds/' + m.route.param('feedId'),
