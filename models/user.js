58df0bcfe72bfe876e5a75b556252aba50eb9cde
David Woolf <davidjohnwoolf@gmail.com>
2015-11-20 13:59:20 -0800

    reformat mongoose models

--- models/user.js
+++ models/user.js
@@ -18,21 +18,15 @@ User.pre('save', function(next) {
   var user = this;
 
   // check if user password is new or modified
-  if (!user.isModified('password')) {
-    return next();
-  }
+  if (!user.isModified('password')) return next();
 
   // generate a salt
   bcrypt.genSalt(10, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
 
     // hash the password combined with the salt
     bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
 
       user.password = hash;
       next();
@@ -42,9 +36,7 @@ User.pre('save', function(next) {
 
 User.methods.comparePassword = function(candidatePassword, done) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) {
-      return done(err);
-    }
+    if (err) return done(err);
 
     done(null, isMatch);
   });
