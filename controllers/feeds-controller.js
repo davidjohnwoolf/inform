563319c60628b68fe72def8c8876bc5a5ec128f9
David Woolf <davidjohnwoolf@gmail.com>
2015-11-20 14:26:13 -0800

    reset default feed if default feed is deleted

--- controllers/feeds-controller.js
+++ controllers/feeds-controller.js
@@ -162,6 +162,12 @@ function destroy(req, res) {
   User.findOne({ _id: req.params.id }, function(err, user) {
     if (err) return res.json(err);
 
+    // if the deleted feed is the default feed, reset default feed
+    // must use ==, not ===, becuase the default feed is a string
+    if (user.defaultFeed && user.feeds.id(req.params.feedId)._id == user.defaultFeed) {
+      user.defaultFeed = '';
+    }
+
     user.feeds.id(req.params.feedId).remove();
 
     user.save(function(err) {
