262d182257c8fea30e9e22cb66d83a45b5ed7aea
David Woolf <davidjohnwoolf@gmail.com>
2016-08-11 21:11:15 -0700

    update styles

--- public/js/src/layout/logged-in-menu.js
+++ public/js/src/layout/logged-in-menu.js
@@ -7,16 +7,13 @@ var LoggedInMenu = {
   view: function(ctrl) {
     return m('div', [
       m('li', [
-        m('a', { href: '/users/' + ctrl.userId, config: m.route }, 'Profile'),
+        m('a', { href: '/users/' + ctrl.userId, config: m.route }, 'Account'),
       ]),
       m('li', [
         m('a', { href: '/users/' + ctrl.userId + '/feeds', config: m.route }, 'Feeds'),
       ]),
       m('li', [
         m('a', { href: '/users/' + ctrl.userId + '/feeds/new', config: m.route }, 'New Feed'),
-      ]),
-      m('li', [
-        m('a', { href: '/users/' + ctrl.userId + '/edit', config: m.route }, 'Edit Account'),
       ]),
       m('li', [
         m('a', { href: '/logout', config: m.route }, 'Logout')
