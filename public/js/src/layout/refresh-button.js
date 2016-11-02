a8a6a9ac8de63668fd6dfe8c3856af691261e1fe
David Woolf <davidjohnwoolf@gmail.com>
2016-11-01 21:19:27 -0700

    add loading animation on click

--- public/js/src/layout/refresh-button.js
+++ public/js/src/layout/refresh-button.js
@@ -3,6 +3,10 @@ var m = require('mithril');
 var RefreshButton = {
   controller: function() {
     var refresh = function() {
+
+      // animate spining icon
+      document.querySelector('#refresh-button .fa-refresh').style.transform = 'rotate(360deg)';
+
       if (!m.route.param('sourceId')) {
         m.route('/users/' + m.route.param('id') + '/feeds/' + m.route.param('feedId'));
       } else {
